6c3f6b10fbde440b5f29b46fba721fd16272ac89
revatighotkar <revatighotkar1999@gmail.com>
2022-05-05 10:06:10 +0530

    test

---
+++ code base1/Codebasetest 1/Codebasetest 1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codebasetest_1
+
+    {
+        internal class program
+        {
+            class Passanger
+            {
+                public String Name { get; set; }
+                public int Age { get; set; }
+                Ticket Ticket { get; set; }
+                public Passanger(String name, int age)
+                {
+                Name = name;
+                    Ticket = new Ticket();
+                    Age = age;
+            }
+                public void TicketBooking(int tickets)
+                {
+                    Ticket.Tickets = tickets;
+                    Console.WriteLine("Ticket Booked Successfull");
+                }
+            }
+            class Ticket
+            {
+                public int tickets;
+                public int Tickets
+
+
+
+                     {
+                    get => tickets;
+                set
+                {
+                    if (value > 5)
+                        throw new Exception("Cannot book more than 5 tickets");
+                    else
+                        tickets = value;
+                }
+            }
+            }
+            static void Main(string[] args)
+            {
+                Passanger passanger = new Passanger("REVATI GHOTKAR", 5);
+                Console.Write("Enter the number of tickets you want to book:");
+                passanger.TicketBooking(int.Parse(Console.ReadLine()));
+                Console.ReadKey();
+            }
+        }
+    }
+
+    
+
+
+
